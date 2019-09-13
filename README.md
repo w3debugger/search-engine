6e3e3b2e6c6b17def4129ed0904210fe37e8f8d2
Umar <w3debugger@gmail.com>
2019-09-13 22:43:22 +0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# search-engine
-Search Engine for JSON files
+# mulk
+
+[![npm](https://badge.fury.io/js/mulk.svg)](https://www.npmjs.com/package/mulk)
+[![](https://img.shields.io/travis/w3debugger/mulk/master.svg?style=flat-square)](http://travis-ci.org/w3debugger/mulk)
+[![](https://img.shields.io/codecov/c/github/w3debugger/mulk.svg?style=flat-square)](http://travis-ci.org/w3debugger/mulk)
+
+
+> Seach Engine for JSON object
+
+## Installation
+> You can either install using `yarn` or using `npm`
+
+```bash
+yarn add search-engine
+npm i search-engine
+```
+
+## Usage
+Import either using `require` or `import`
+
+```javascript
+const searchEngine = require('search-engine');
+
+searchEngine(JSON_Object, field_to_search);
+
+// Search by key
+searchEngine(JSON_Object, '@key:value');
+```
+
+Feel free to open an issue if you need help with some specific usecase.
+
+## Contributions
+
+* Report issues with problems and suggestions
+* Open pull request with improvements
+* Spread the word
+* Reach out with any feedback [![Twitter URL](https://img.shields.io/twitter/url/https/twitter.com/w3debugger.svg?style=social&label=Follow%20%40w3debugger)](https://twitter.com/w3debugger)
+
+## License
+
+MIT &copy; [Muhammad Umar Mehmood](https://twitter.com/w3debugger)
---
+++ __tests__/index.js
@@ -0,0 +1,132 @@
+const searchEngine = require('../index');
+const users = require('./users');
+
+test('search within the whole JSON', () => {
+  const expectedFieldValue = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "tags": [
+        "Programmer",
+        "Entrepreneur",
+        "Writer",
+        "Political Organizer",
+        "Internet Hacktivist"
+      ],
+      "company": [
+        {
+          "name": "Romaguera-Crona 1",
+          "catchPhrase": "Multi-layered client-server neural-net 1",
+          "bs": "harness real-time e-markets 1"
+        }, {
+          "name": "Romaguera-Crona 2",
+          "catchPhrase": "Multi-layered client-server neural-net 2",
+          "bs": "harness real-time e-markets 2"
+        }, {
+          "name": "Romaguera-Crona 3",
+          "catchPhrase": "Multi-layered client-server neural-net 3",
+          "bs": "harness real-time e-markets 3"
+        }
+      ]
+    }
+  ];
+
+  const searchKeywords = [
+    "Leanne Graham", // search in level 1
+    "hildegard.org", // search in level 2
+    "Political Organizer", // search in simple array
+    "Romaguera-Crona 2" // search in array of object
+  ]
+
+  searchKeywords.forEach(testItem => {
+    const actualFieldValue = searchEngine(users, testItem);
+
+    expect(actualFieldValue).toEqual(expectedFieldValue);
+  });
+});
+
+test('search by value by specific key within JSON', () => {
+  const expectedFieldValue = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "tags": [
+        "Programmer",
+        "Entrepreneur",
+        "Writer",
+        "Political Organizer",
+        "Internet Hacktivist"
+      ],
+      "company": [
+        {
+          "name": "Romaguera-Crona 1",
+          "catchPhrase": "Multi-layered client-server neural-net 1",
+          "bs": "harness real-time e-markets 1"
+        }, {
+          "name": "Romaguera-Crona 2",
+          "catchPhrase": "Multi-layered client-server neural-net 2",
+          "bs": "harness real-time e-markets 2"
+        }, {
+          "name": "Romaguera-Crona 3",
+          "catchPhrase": "Multi-layered client-server neural-net 3",
+          "bs": "harness real-time e-markets 3"
+        }
+      ]
+    }
+  ];
+
+  const searchKeywords = [
+    "@name:Leanne Graham", // search in level 1
+    "@city:Gwenborough", // search in level 2
+    "@bs:harness real-time e-markets 2" // search in array
+  ]
+
+  searchKeywords.forEach(testItem => {
+    const actualFieldValue = searchEngine(users, testItem);
+
+    expect(actualFieldValue).toEqual(expectedFieldValue);
+  });
+});
+
+test('If nothing found', () => {
+  const expectedFieldValue = [];
+
+  const searchKeywords = [
+    "EmptyField",
+    "@missingField:Leanne Graham"
+  ]
+
+  searchKeywords.forEach(testItem => {
+    const actualFieldValue = searchEngine(users, testItem);
+
+    expect(actualFieldValue).toEqual(expectedFieldValue);
+  });
+});
